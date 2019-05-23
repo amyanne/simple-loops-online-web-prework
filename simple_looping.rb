769614281e0f414ab1340995452aef9a7774d7ce
Amy Westcott <amy.westcott@yahoo.com>
2019-05-23 19:35:40 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -38,7 +38,7 @@ def until_iterator(number_of_times)
   # hint: use a counter to tell the until loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter == 0
-  until counter == 8 
+  until counter == 6
     counter += 1 
     puts phrase 
   end
